c970fcbd20d5d1dfafb20df79ba8b09f9af393a5
Miteos1986 <mdmitriy1986@gmail.com>
2024-02-19 18:02:39 +0100

    финиш
    изменено дз1

--- js/task-1.js
+++ js/task-1.js
@@ -22,13 +22,10 @@
 
 function isEnoughCapacity(products, containerSize) {
   let sum = 0;
-  for (let product of products) {
-    sum += Object.values(product)[0];
-    if (sum <= containerSize) {
-      return true;
-    }
+  for (let product of Object.values(products)) {
+    sum += product;
   }
-  return false;
+  return sum <= containerSize;
 }
 
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
--- js/task-3.js
+++ js/task-3.js
@@ -5,9 +5,18 @@
 const profile = {
   username: 'Jacob',
   playTime: 300,
-};
-getInfo = ;
 
+  changeUsername(newName) {
+    this.username === newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 // Доповни об’єкт profile методами для роботи з його властивостями.
 
 // Метод changeUsername(newName) повинен приймати рядок (нове ім’я) в параметр newName та змінювати значення властивості username на нове. Нічого не повертає.
